24a2c8a014b383944cfeb73fce5b7f1ec072f0f7
Joshua <git@timesmith.ca>
2023-06-15 05:11:06 -0600

    Updated SPM version tag for Xcode 14.2

--- Package.swift
+++ Package.swift
@@ -14,21 +14,21 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Esri/arcgis-runtime-ios/", exact: "100.15.2"),  // Mapping and GIS functionality
-        .package(url: "https://github.com/beMappy/CocoaLumberjackSwift/", exact: "3.7.4-xc14-3"), // Powerful & flexible logging framework
-        .package(url: "https://github.com/beMappy/Mappy-KeychainAccess/", exact: "4.2.2"), // Simple Swift wrapper for Keychain
-        .package(url: "https://github.com/beMappy/Swinject/", exact: "2.8.2"), // A lightweight dependency injection framework.
-        .package(url: "https://github.com/beMappy/ZIPFoundation/", exact: "0.9.15"), // zip and unzip items
-        .package(url: "https://github.com/beMappy/Reachability.swift/", exact: "5.1.0-xc14.2-1"), // To check Internet connection status
-        .package(url: "https://github.com/beMappy/PusherSwift/", exact: "10.1.1-xc14.2-2"), // Messaging infrastructure
-        .package(url: "https://github.com/beMappy/RealmSwift/", exact: "10.39.1-d3"), // Database
+        .package(url: "https://github.com/beMappy/CocoaLumberjackSwift/", exact: "3.7.4-SPM2"), // Powerful & flexible logging framework
+        .package(url: "https://github.com/beMappy/Mappy-KeychainAccess/", exact: "4.2.2-SPM"), // Simple Swift wrapper for Keychain
+        .package(url: "https://github.com/beMappy/Swinject/", exact: "2.8.2-SPM"), // A lightweight dependency injection framework.
+        .package(url: "https://github.com/beMappy/ZIPFoundation/", exact: "0.9.15-SPM"), // zip and unzip items
+        .package(url: "https://github.com/beMappy/Reachability.swift/", exact: "5.1.0-SPM"), // To check Internet connection status
+        .package(url: "https://github.com/beMappy/PusherSwift/", exact: "10.1.1-SPM"), // Messaging infrastructure
+        .package(url: "https://github.com/beMappy/RealmSwift", exact: "10.39.1-SPM"),
     ],
     targets: [
         .target(
             name: "MappyWrapper",
             dependencies: [
                 .target(name: "Mappy"),
-                .product(name: "CocoaLumberjackSwift", package: "CocoaLumberjackSwift"),
                 .product(name: "ArcGIS", package: "arcgis-runtime-ios"),
+                .product(name: "CocoaLumberjackSwift", package: "CocoaLumberjackSwift"),
                 .product(name: "KeychainAccess", package: "Mappy-KeychainAccess"),
                 .product(name: "Swinject", package: "Swinject"),
                 .product(name: "ZIPFoundation", package: "ZIPFoundation"),
