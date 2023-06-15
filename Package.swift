71d1d14a4bd5b96919d042769baa6e42ef3e6af5
Joshua <git@timesmith.ca>
2023-06-15 01:51:47 -0600

    Updated to explicit versions, for distribution

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -9,33 +8,21 @@ let package = Package(
         .iOS(.v14)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Mappy",
             targets: ["MappyWrapper", "Mappy"]),
     ],
     dependencies: [
         .package(url: "https://github.com/Esri/arcgis-runtime-ios/", exact: "100.15.2"),  // Mapping and GIS functionality
-        .package(url: "https://github.com/beMappy/CocoaLumberjackSwift/", branch: "SPM"), // Powerful & flexible logging framework
-        .package(url: "https://github.com/beMappy/Mappy-KeychainAccess/", branch: "SPM"), // Simple Swift wrapper for Keychain
-        .package(url: "https://github.com/beMappy/Swinject/", branch: "SPM"), // A lightweight dependency injection framework.
-        .package(url: "https://github.com/beMappy/ZIPFoundation/", branch: "SPM"), // zip and unzip items
-        .package(url: "https://github.com/beMappy/Reachability.swift/", branch: "SPM"), // To check Internet connection status
-        .package(url: "https://github.com/beMappy/PusherSwift/", branch: "SPM"), // Messaging infrastructure
-        .package(url: "https://github.com/beMappy/RealmSwift/", branch: "SPM"), // Database
-        /*
         .package(url: "https://github.com/beMappy/CocoaLumberjackSwift/", exact: "3.7.4-xc14-3"), // Powerful & flexible logging framework
         .package(url: "https://github.com/beMappy/Mappy-KeychainAccess/", exact: "4.2.2"), // Simple Swift wrapper for Keychain
         .package(url: "https://github.com/beMappy/Swinject/", exact: "2.8.2"), // A lightweight dependency injection framework.
         .package(url: "https://github.com/beMappy/ZIPFoundation/", exact: "0.9.15"), // zip and unzip items
         .package(url: "https://github.com/beMappy/Reachability.swift/", exact: "5.1.0-xc14.2-1"), // To check Internet connection status
         .package(url: "https://github.com/beMappy/PusherSwift/", exact: "10.1.1-xc14.2-2"), // Messaging infrastructure
-        .package(url: "https://github.com/beMappy/RealmSwift/", exact: "10.39.1"), // Database
-         */
+        .package(url: "https://github.com/beMappy/RealmSwift/", exact: "10.39.1-d3"), // Database
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MappyWrapper",
             dependencies: [
