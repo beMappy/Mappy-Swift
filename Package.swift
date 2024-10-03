22ec96873b3ebee6f08656c0af398c8bff994c71
Earl <git@timesmith.ca>
2024-10-03 15:38:28 -0600

    chore: SPM dependencies update

--- Package.swift
+++ Package.swift
@@ -18,9 +18,7 @@ let package = Package(
         .package(url: "https://github.com/beMappy/Mappy-KeychainAccess/", exact: "4.2.2-SPM"), // Simple Swift wrapper for Keychain
         .package(url: "https://github.com/beMappy/Swinject/", exact: "2.8.2-SPM"), // A lightweight dependency injection framework.
         .package(url: "https://github.com/beMappy/ZIPFoundation/", exact: "0.9.15-SPM"), // zip and unzip items
-        .package(url: "https://github.com/beMappy/Reachability.swift/", exact: "5.1.0-SPM"), // To check Internet connection status
         .package(url: "https://github.com/beMappy/PusherSwift/", exact: "10.1.1-SPM"), // Messaging infrastructure
-        .package(url: "https://github.com/beMappy/RealmSwift", exact: "10.39.1-SPM"),
     ],
     targets: [
         .target(
@@ -32,9 +30,7 @@ let package = Package(
                 .product(name: "KeychainAccess", package: "Mappy-KeychainAccess"),
                 .product(name: "Swinject", package: "Swinject"),
                 .product(name: "ZIPFoundation", package: "ZIPFoundation"),
-                .product(name: "Reachability", package: "Reachability.swift"),
                 .product(name: "PusherSwift", package: "PusherSwift"),
-                .product(name: "RealmSwift", package: "RealmSwift"),
             ]),
         .binaryTarget(name: "Mappy", path: "./Mappy.xcframework"),
     ]
